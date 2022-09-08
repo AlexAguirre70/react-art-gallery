96f4c3abf70273328f83ddc02d207bb8112a73e2
Alex Aguirre <teamaguirre@gmail.com>
2022-09-07 19:50:13 -0700

    Created the initial framework for the app

---
+++ src/ButtonBar.js
@@ -0,0 +1,10 @@
+import React,{useState,useEffect} from "react";
+
+function ButtonBar(){
+    return(
+        <h1>Button Bar Placeholder</h1>
+    )
+}
+
+
+export default ButtonBar
---
+++ src/Gallery.js
@@ -0,0 +1,11 @@
+import React, {useState,useEffect} from "react";
+
+function Gallery() {
+    return(
+        <h1> Gallery Placeholder</h1>
+
+
+    )
+}
+
+export default Gallery
