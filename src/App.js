42d0ad8447178e390c89b20689abba34e28b2646
Alex Aguirre <teamaguirre@gmail.com>
2022-09-24 12:35:20 -0700

    Added the light and mode toggle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Gallery from './Gallery'
 import ButtonBar from './ButtonBar'
+import { useSelector,useDispatch } from 'react-redux';
+import { darkMode,lightMode} from './features/modelSlice';
 import { useState,useEffect } from 'react';
 
 const App=()=> {
   let [data,setData]= useState({})
   let [artId,setartId] =useState(12720)
+  const dispatch =useDispatch()
+  const mode = useSelector((state)=>state.mode)
+
+  const toggleMode =()=> {
+    mode.darkMode ? dispatch(lightMode()): dispatch(darkMode())
+  }
 
   useEffect (() =>{
     document.title = 'Welcome to Artworld'
@@ -17,10 +25,11 @@ const App=()=> {
     setartId(artId+Number(e.target.value))
   }
   return (
-    <div className="App">
+    <div className="App" style={{backgroundColor:mode.color1, color:'white'}}>
     <Gallery objectImg={data.primaryImage} artist={data.artistDisplayName} title={data.title} />
   
     <ButtonBar handleIterate={handleIterate}/>
+    <button onClick={toggleMode}>{mode.darkMode ? 'Ligth Mode' : 'Dark Mode'}</button>
     </div>
   );
 }
--- src/Store.js
+++ src/Store.js
@@ -1,10 +1,12 @@
 
 import { configureStore } from '@reduxjs/toolkit'
 import dataReducer from './features/dataSlice'
+import modeReducer from './features/modelSlice'
 
 export const store = configureStore({
     reducer: {
-        data:dataReducer
+        data:dataReducer,
+        mode: modeReducer
 
     }
 })
